4e29d7bdcbfb2eafc5d2cd535b3a40f6a1845156
T. Morrison <thomas.r.morrison@gmail.com>
2017-08-08 08:50:12 -0400

    add portalEcosystem

--- portalEcosystem/server.R
+++ portalEcosystem/server.R
@@ -6,7 +6,7 @@ library(data.table)
 library(DT)
 
 
-#Data use cases
+#Data portals
 
 #use <- read.csv('C:/Users/TRM/Documents/GitHub/R projects/NRGI/Shiny app/Ecosystem3/Ecocsystem3/useCases.csv', stringsAsFactors = FALSE)
 #portal <- read.csv('C:/Users/TRM/Documents/GitHub/R projects/NRGI/Shiny app/Ecosystem3/Ecocsystem3/dataPortals2.csv', stringsAsFactors = FALSE)
@@ -18,12 +18,12 @@ library(DT)
 
 ##Google sheets
 #portal <- read.csv("https://docs.google.com/spreadsheets/d/1q1tvFxDmDK_neBBhx73mPjJRZ8H5UZfp4Ys88eY703c/pub?gid=1584017053&single=true&output=csv", stringsAsFactors=FALSE)
+
+portal <- read.csv("https://docs.google.com/spreadsheets/d/1aQNFphK8yS9ntV898Y8YN-zo-EdH-H-_Yrypy1u-lWQ/pub?gid=0&single=true&output=csv", stringsAsFactors = FALSE)
 #https://docs.google.com/spreadsheets/d/1q1tvFxDmDK_neBBhx73mPjJRZ8H5UZfp4Ys88eY703c/pub?gid=1584017053&single=true&output=csv
 #https://docs.google.com/spreadsheets/d/1q1tvFxDmDK_neBBhx73mPjJRZ8H5UZfp4Ys88eY703c/pub?output=csv
 
 #use <- read.csv("https://docs.google.com/spreadsheets/d/1ym3cHEvCZRPnYd15HTiH75pVjysr-VPzqD_gdzbwlHE/pub?gid=2037077924&single=true&output=csv", stringsAsFactors=FALSE)
-
-use <- read.csv("https://docs.google.com/spreadsheets/d/1b4Ok08F8iBQFc8jDhsbnUSEc1kjHwwB2mFLKCj5-GbE/pub?gid=0&single=true&output=csv", stringsAsFactors = FALSE)
 
 ##AWS only
 
@@ -32,39 +32,40 @@ use <- read.csv("https://docs.google.com/spreadsheets/d/1b4Ok08F8iBQFc8jDhsbnUSE
 ###
 
 
-setDT(use)#; setDT(portal)
+setDT(portal)
 
-use <- use[order(Project)]
-#portal <- portal[order(Project)]
+#use <- use[order(Project)]
+portal <- portal[order(Project)]
 
-attr(use,"name") <- "use"
-#attr(portal,"name") <- "portal"
+#attr(use,"name") <- "use"
+attr(portal,"name") <- "portal"
 
-use$Data.type2 <- paste(lapply(strsplit(use$Data.type, ','), function(x){paste0('<li>',x,'</li>', collapse='')}))
-use$Data.type <- paste(lapply(strsplit(use$Data.type,','), paste0, "<br>", collapse=''))
-use$Product.type <- paste(lapply(strsplit(use$Product.type,','), paste0, "<br>", collapse=''))
-use$Methodology <- paste(lapply(strsplit(use$Methodology,','), paste0, "<br>", collapse=''))
-use$Project <- paste0('<a target="_blank" href="',use$Link,'">',use$Project,'</a>')
-
-use$Case.study
-
-use[, Case.study := ifelse(Case.study=="", "", paste0('<a target="_blank" href="',Case.study,'">',"Link",'</a>'))]
-
-paste0('<a target="_blank" href="',use$`Case Study`,'">',"Link",'</a>')
-
-use <- use[, c("Project","Data.type2","Product.type","Country","Organization","Year","Overview","Case.study", "Data.type"), with=FALSE]
-names(use) <- c("Project","Data Type","Product Type","Country","Organization","Year","Overview","Case Study", "Data.type")
-use <- use[, c("Project","Country","Organization","Data Type","Product Type","Year","Overview", "Case Study","Data.type")]
+# use$Data.type2 <- paste(lapply(strsplit(use$Data.type, ','), function(x){paste0('<li>',x,'</li>', collapse='')}))
+# use$Data.type <- paste(lapply(strsplit(use$Data.type,','), paste0, "<br>", collapse=''))
+# use$Product.type <- paste(lapply(strsplit(use$Product.type,','), paste0, "<br>", collapse=''))
+# use$Methodology <- paste(lapply(strsplit(use$Methodology,','), paste0, "<br>", collapse=''))
+# use$Project <- paste0('<a target="_blank" href="',use$Link,'">',use$Project,'</a>')
 # 
-# portal$Data.type2 <- paste(lapply(strsplit(portal$Data.type, ','), function(x){paste0('<li>',x,'</li>', collapse='')}))
-# portal$Data.type <- paste(lapply(strsplit(portal$Data.type,','), paste0, "<br>", collapse=''))
-# portal$Data.format <- paste(lapply(strsplit(portal$Data.format,','), paste0, "<br>", collapse=''))
-# portal$Methodology <- paste(lapply(strsplit(portal$Methodology,','), paste0, "<br>", collapse=''))
-# portal$Project <- paste0('<a target="_blank" href="',portal$Link,'">',portal$Project,'</a>')
+# use$Case.study
 # 
+# use[, Case.study := ifelse(Case.study=="", "", paste0('<a target="_blank" href="',Case.study,'">',"Link",'</a>'))]
 # 
-# portal <- portal[, c("Project","Data.type2","Data.format","Data.license","Country","Organization","Year","Overview", "Data.type"), with=FALSE]
-# names(portal) <- c("Project","Data Type","Data Availability","Data License","Country","Organization","Year","Overview", "Data.type")
+# paste0('<a target="_blank" href="',use$`Case Study`,'">',"Link",'</a>')
+# 
+# use <- use[, c("Project","Data.type2","Product.type","Country","Organization","Year","Overview","Case.study", "Data.type"), with=FALSE]
+# names(use) <- c("Project","Data Type","Product Type","Country","Organization","Year","Overview","Case Study", "Data.type")
+# 
+
+portal$Data.type2 <- paste(lapply(strsplit(portal$Data.type, ','), function(x){paste0('<li>',x,'</li>', collapse='')}))
+portal$Data.type <- paste(lapply(strsplit(portal$Data.type,','), paste0, "<br>", collapse=''))
+portal$Data.format <- paste(lapply(strsplit(portal$Data.format,','), paste0, "<br>", collapse=''))
+portal$Methodology <- paste(lapply(strsplit(portal$Methodology,','), paste0, "<br>", collapse=''))
+portal$Project <- paste0('<a target="_blank" href="',portal$Link,'">',portal$Project,'</a>')
+
+
+portal <- portal[, c("Project","Data.type2","Data.format","Data.license","Country","Organization","Year","Overview", "Data.type"), with=FALSE]
+names(portal) <- c("Project","Data Type","Data Availability","Data License","Country","Organization","Year","Overview", "Data.type")
+portal <- portal[, c("Project","Country","Organization","Data Type", "Data Availability","Year","Overview", "Data License","Data.type")]
 
 ################################################################################################################################
 ################################################################################################################################
@@ -76,34 +77,33 @@ shinyServer(function(input, output) {
   observeEvent(input$show, {
     showModal(modalDialog(
       title = "FAQs",
-      HTML("Welcome to NRGI's Data Use Case Ecosystem Explorer. <br><br>
+      HTML("Welcome to NRGI's Data Portal Explorer. <br><br>
            
-           This tool showcases the different uses of data in extractive governance projects from around the globe. It has been designed with an emphasis on the types of
-           data a project uses and what the final product is, so that other stakeholders may find examples which they can follow and build upon. <br><br>
+           This tool showcases the different places around the web where stakeholders can access data on the extractives industry. <br><br>
            
-           Data use cases are projects that use extractive data to answer or pose a question. You can search the catalogue of examples by country, type of data used 
-           and type of final product.<br><br>
+           Data portals allow users to download or view data on a variety of extractive industry topics, including concession areas, company ownership and payments 
+           to governments. You can search for different data portals by country, data type and data availability. <br><br>
            
-           There are currently over 40 projects in the data ecosystem. NRGI will be updating the database periodically and appreciates any suggestions for additions. Please email info@resourcegovernance.org with suggestions or comments.")      ))
+           There are currently over 30 data portals in the data ecosystem. NRGI will be updating the database periodically and appreciates any suggestions for additions. Please email info@resourcegovernance.org with suggestions or comments.")      ))
   })
   
   dat <- reactive({
-    use
+    portal
   })
   
-  # column_names <- reactive({
-  #   if(input$data_select=="portals") {
-  #     #return(c("Butt"=1, "Data Type"=2, "Product Type"=3,"Whatever"=4))
-  #     c("PROJECT","Data Type","Data Format","Data License","Country","Organization","Year","Overview","Data.type")
-  #   } else {
-  #     #return(c("Project"=1, "Data Type"=2, "Data Format"=3, "Data License"=4))
-  #     
-  #   }
-  # })
+  column_names <- reactive({
+    if(input$data_select=="portals") {
+      #return(c("Butt"=1, "Data Type"=2, "Product Type"=3,"Whatever"=4))
+      c("PROJECT","Data Type","Data Format","Data License","Country","Organization","Year","Overview","Data.type")
+    } else {
+      #return(c("Project"=1, "Data Type"=2, "Data Format"=3, "Data License"=4))
+      
+    }
+  })
   
-  # second_checkbox <- reactive({
-  #   "Select a product type"
-  # })
+  second_checkbox <- reactive({
+    "Data availability"
+    })
   
   output$dataChoices <- renderUI({
     datas <- unique(unlist(strsplit(paste(dat()[!(Data.type%in%c(""," ","<br>"," <br>"))]$Data.type, collapse=''), split='<br>')))
@@ -120,15 +120,12 @@ shinyServer(function(input, output) {
   
   output$productChoices <- renderUI({
     #datas <- unique(gsub("<br>","", unlist(strsplit(paste(dat()$Product.type, collapse=''), split='<br>'))))
-    #datas <- unique(gsub("<br>","", unlist(strsplit(paste(dat()[!(dat()[[3]]%in%c(" ","","<br>"))][[3]], collapse=''), split='<br>'))))
-    
-    datas <- unique(gsub("<br>","", unlist(strsplit(paste(dat()[!(dat()[["Product Type"]]%in%c(" ","","<br>"))][["Product Type"]], collapse=''), split='<br>'))))
-    
+    datas <- unique(gsub("<br>","", unlist(strsplit(paste(dat()[!(dat()[["Data Availability"]]%in%c(" ","","<br>"))][["Data Availability"]], collapse=''), split='<br>'))))
     # checkboxGroupInput(inputId="product.type.check",
     #                    choices=datas[order(datas)],
     #                    label=second_checkbox())#"Select a product type")
     selectizeInput(inputId = "product.type.check",
-                   label="Select one or more product types",
+                   label="Select a product type",
                    choices=datas[order(datas)],
                    multiple=TRUE,
                    selected=NULL)
@@ -176,7 +173,7 @@ shinyServer(function(input, output) {
   productTypeList <- reactive({
     a <- list()
     for(i in 1:length(productType())) {
-      a[[i]] <- grep(productType()[i], dat()[["Product Type"]])#dat()$Product.type)
+      a[[i]] <- grep(productType()[i], dat()[["Data Availability"]])#dat()$Product.type)
     }
     a
   })
@@ -227,14 +224,14 @@ shinyServer(function(input, output) {
                  columnDefs = list(list(className = 'dt-bottom', targets = 0:4),
                                    list(className = 'col-md-3 col-sm-3', targets = 6),
                                    #list(className = '', targets = 6),
-                                   #list(className = 'dt-head-center', targets = 0:7),
+                                   list(className = 'dt-head-center', targets = 0:7),
+                                   list(className = 'dt-center', targets = 5),
                                    #list(width='100px', targets=1:2),
-                                   list(width='50px', targets=5),
+                                   #list(width='50px', targets=5),
                                    list(width='100px', targets=7),
-                                   list(width='100px', targets=0:4)),
+                                   list(width='100px', targets=0:5)),
                  initComplete = JS(
                    "function(settings, json) {",
-                   "$(this.api().table().body()).css({'text-align': 'left', 'vertical-align':'top'});",
                    "$(this.api().table().body()).css({'text-align': 'left', 'vertical-align':'top'});",
                    "$(this.api().table().header()).css({'background-color': '#ff6e49', 'color': '#fff'});",
                    "}")
---
+++ portalEcosystem/ui.R
@@ -0,0 +1,45 @@
+
+library(shiny)
+library(shinythemes)
+library(shinyjs)
+
+shinyUI(fluidPage(
+  useShinyjs(),
+  theme= "nrgi-ecosystems-portal.css",
+  
+  # Application title
+   ##titlePanel("Data Portal Explorer"),
+  
+  # Sidebar
+  sidebarLayout(
+    
+    sidebarPanel(      
+      width=2,   
+      # selectInput(inputId="data_select",
+      #             label="Select an ecosystem",
+      #             choices=c("Data portals"="portals","Data use cases"="uses")),
+      # #actionButton(inputId="show",
+      #             label="Click for Info"),
+      uiOutput("countryChoices"),
+      uiOutput("dataChoices"),
+      uiOutput("productChoices")
+      
+    ),
+    # Main
+    mainPanel(
+      
+      actionButton(inputId="show",
+                   label=HTML('<span class="glyphicon glyphicon-question-sign"></span>')),
+      width=10,
+      #tableOutput("sample"),
+      DT::dataTableOutput("data.type"),
+      textOutput("textout")
+    )
+  )
+)
+)
+
+
+
+
+#add country
